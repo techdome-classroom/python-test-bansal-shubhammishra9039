2aea76b2f0f29991b08968f70c9d6b71d61f5a60
Shubham Mishra <shubhammishra6595@gmail.com>
2024-02-23 13:36:53 +0530

    done

--- program1.py
+++ program1.py
@@ -1,13 +1,16 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+    def is_valid_parentheses(self, s):
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
 
-
-
-  
-
+solution = Solution()
+print(solution.is_valid_parentheses("(){}[]"))  
+print(solution.is_valid_parentheses("([)]"))
